a0b1d63f653763fc840df86b669e1aa35815b365
guillaumepicard <guillaumepicard99@gmail.com>
2023-09-18 11:32:05 -0400

    map import unity

--- Assets/JRPG/Tiled/maps/BaseMap.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="BaseMap" tilewidth="32" tileheight="32" tilecount="725" columns="29">
- <image source="../../../World of Solaria Rural Village Update 05a/32x32/Tilesets/Rural Village Old Tiles32 (No Longer Supported).png" width="928" height="800"/>
-</tileset>

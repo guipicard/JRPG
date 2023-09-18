acf3d88190853deccd00a71f19c640fe97167f88
Guillaume Picard <guillaumepicard99@gmail.com>
2023-09-18 07:18:59 -0400

    first map

---
+++ Assets/JRPG/Tiled/maps/tilesBase.tsx
@@ -0,0 +1,175 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="tilesBase" tilewidth="32" tileheight="32" tilecount="725" columns="29">
+ <image source="../JRPG/Assets/World of Solaria Rural Village Update 05a/32x32/Tilesets/Rural Village Old Tiles32 (No Longer Supported).png" width="928" height="800"/>
+ <tile id="112">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="16.0909" y="10.0909" width="15.8182" height="21.8182"/>
+  </objectgroup>
+ </tile>
+ <tile id="113">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="10" width="32" height="22.0909"/>
+  </objectgroup>
+ </tile>
+ <tile id="114">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="10" width="16.0909" height="22"/>
+  </objectgroup>
+ </tile>
+ <tile id="122">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="15.9915" y="18.6284">
+    <ellipse/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="123">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="15.9915" y="18.033">
+    <ellipse/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="141">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="16" y="0" width="16" height="31.9091"/>
+  </objectgroup>
+ </tile>
+ <tile id="143">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="32.0909"/>
+  </objectgroup>
+ </tile>
+ <tile id="170">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="16" y="0.0909091" width="16" height="7.63636"/>
+  </objectgroup>
+ </tile>
+ <tile id="172">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.0909091" y="0.0909091" width="15.8182" height="7.81818"/>
+  </objectgroup>
+ </tile>
+ <tile id="180">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="16.1616" y="18.5433">
+    <ellipse/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="181">
+  <objectgroup draworder="index" id="3">
+   <object id="2" x="0.0850611" y="0.0850611" width="31.8129" height="31.7278"/>
+   <object id="3" x="0.170122" y="0.0850611" width="31.7278" height="31.7278"/>
+   <object id="4" x="0" y="0" width="32" height="32"/>
+   <object id="6" x="16.0766" y="17.6927">
+    <ellipse/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="357">
+  <objectgroup draworder="index" id="5">
+   <object id="6" x="0" y="21.7273" width="31.9091" height="10.2727"/>
+  </objectgroup>
+ </tile>
+ <tile id="358">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="22" width="31.9091" height="10"/>
+  </objectgroup>
+ </tile>
+ <tile id="359">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.181818" y="22" width="31.7273" height="10"/>
+  </objectgroup>
+ </tile>
+ <tile id="386">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="11.8182" height="32.0909"/>
+  </objectgroup>
+ </tile>
+ <tile id="388">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="20" y="0.0909091" width="11.9091" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="415">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0.0909091" width="11.8182" height="31.7273"/>
+   <object id="2" x="0.0909091" y="22" width="31.8182" height="9.81818"/>
+  </objectgroup>
+ </tile>
+ <tile id="417">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="22" width="31.9091" height="10"/>
+   <object id="2" x="20" y="0.0909091" width="11.7273" height="31.9091"/>
+  </objectgroup>
+ </tile>
+ <tile id="510">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.0909091" y="0" width="31.9091" height="31.9091"/>
+  </objectgroup>
+ </tile>
+ <tile id="522">
+  <animation>
+   <frame tileid="522" duration="600"/>
+   <frame tileid="266" duration="400"/>
+   <frame tileid="522" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="523">
+  <animation>
+   <frame tileid="523" duration="600"/>
+   <frame tileid="267" duration="400"/>
+   <frame tileid="523" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="524">
+  <animation>
+   <frame tileid="524" duration="600"/>
+   <frame tileid="268" duration="400"/>
+   <frame tileid="524" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="526">
+  <animation>
+   <frame tileid="526" duration="600"/>
+   <frame tileid="270" duration="400"/>
+   <frame tileid="526" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="551">
+  <animation>
+   <frame tileid="551" duration="600"/>
+   <frame tileid="295" duration="400"/>
+   <frame tileid="551" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="553">
+  <animation>
+   <frame tileid="553" duration="600"/>
+   <frame tileid="297" duration="400"/>
+   <frame tileid="553" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="580">
+  <animation>
+   <frame tileid="580" duration="600"/>
+   <frame tileid="324" duration="400"/>
+   <frame tileid="580" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="581">
+  <animation>
+   <frame tileid="581" duration="600"/>
+   <frame tileid="325" duration="400"/>
+   <frame tileid="581" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="582">
+  <animation>
+   <frame tileid="582" duration="600"/>
+   <frame tileid="326" duration="400"/>
+   <frame tileid="582" duration="200"/>
+  </animation>
+ </tile>
+</tileset>

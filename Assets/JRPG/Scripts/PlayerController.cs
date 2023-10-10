ca2d95c8c5aebeecfa0646251f704f8eea47c713
2230134 <2230134@bart.ca>
2023-10-10 12:08:01 -0400

    mage anim + movement

--- Assets/JRPG/Scripts/PlayerController.cs
+++ Assets/JRPG/Scripts/PlayerController.cs
@@ -6,8 +6,10 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private SaveData saveData;
+    [SerializeField] private Animator animator;
 
-   
+    private bool isRotated = false;
+    private bool moving = false;
 
     public void Save()
     {
@@ -26,13 +28,78 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        moving = false;
         if (Input.GetKey(KeyCode.D)) 
         {
             Vector2 pos = transform.position;
 
-            pos.x += 10.0f * Time.deltaTime;
+            pos.x += 1.0f * Time.deltaTime;
 
             transform.position = pos;
+
+            if(!isRotated)
+            {
+                transform.Rotate(0, 180, 0);
+                isRotated = true;
+            }
+
+            animator.SetBool("Walking", true);
+            animator.SetBool("WalkingDown", false);
+            animator.SetBool("WalkingUp", false);
+            moving = true;
         }
+        if (Input.GetKey(KeyCode.A))
+        {
+            Vector2 pos = transform.position;
+
+            pos.x -= 1.0f * Time.deltaTime;
+
+            transform.position = pos;
+
+            animator.SetBool("Walking", true);
+            animator.SetBool("WalkingDown", false);
+            animator.SetBool("WalkingUp", false);
+            if (isRotated)
+            {
+                transform.Rotate(0, 180, 0);
+                isRotated = false;
+            }
+            moving = true;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            Vector2 pos = transform.position;
+
+            pos.y -= 1.0f * Time.deltaTime;
+
+            transform.position = pos;
+
+            animator.SetBool("WalkingDown", true);
+            animator.SetBool("Walking", false);
+            moving = true;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            Vector2 pos = transform.position;
+
+            pos.y += 1.0f * Time.deltaTime;
+
+            transform.position = pos;
+
+            animator.SetBool("WalkingUp", true);
+            animator.SetBool("Walking", false);
+            moving = true;
+        }
+        if(Input.GetKeyDown(KeyCode.E)) 
+        {
+            animator.SetTrigger("Interact");
+        }
+        if(!moving)
+        {
+            animator.SetBool("WalkingUp", false);
+            animator.SetBool("WalkingDown", false);
+            animator.SetBool("Walking", false);
+        }
+
     }
 }
